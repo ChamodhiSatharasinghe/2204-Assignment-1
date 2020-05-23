c0b704545a3126f4658c2f43fb8ec1bce0977830
Chimi <you@example.com>
2020-05-23 23:50:46 +0530

    assignment 1 question 3

--- question 2/volume.scala
+++ question 2/volume.scala
@@ -1,8 +1,8 @@
 object volume extends App{
 
-var pi : Double = 22/7;
+var pi : Double = 22.00/7.00;
 
-def calVol(rad: Double) = (4/3)*(pi)*(scala.math.pow(rad,3));
+def calVol(rad: Double) = (4.00/3.00)*(pi)*(scala.math.pow(rad,3));
 
 println("# Volume of the sphere with radius 5 :- " + calVol(5));
 
